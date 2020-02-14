b0f6b22025b8bb2d5ffc1e57f6a86da982555d91
santi12dsd <60608518+santi12dsd@users.noreply.github.com>
2020-02-14 09:43:03 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota; 
+	var maximo = 10;
+	var minimo = 1;
+
+	nota = Math.floor(Math.random()*((maximo + 1)- minimo) + minimo );
+    if (nota >= 9) {
+		alert( nota  + " EXELENTE");
+	}
+	else if(nota >=4) {
+		alert( nota  + " APROBÓ");
+	}
+	else {
+		alert( nota  + " Vamos, la proxima se puede" );
+	}
 
 }//FIN DE LA FUNCIÓN
