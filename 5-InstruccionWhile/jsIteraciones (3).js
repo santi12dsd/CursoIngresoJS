34c3cb7553671d8a2684ebdf314579a6c335ec2d
santi12dsd <60608518+santi12dsd@users.noreply.github.com>
2020-02-17 10:36:26 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,16 @@
-function mostrar()
-{
+function mostrar() {
+    // Pido el dato
+    var clave;
+    clave = prompt("ingrese el número clave.");
 
-var clave = prompt("ingrese el número clave.");
+    while(clave != "utn750") {
+        clave = prompt("Clave incorrecta, reintente: ");
+
+    }
+    alert("Contraseña correcta");
+    
+
+
 
 
 }//FIN DE LA FUNCIÓN
