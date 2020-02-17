c50c014fdc3568ee701cdf2af1e65d2a0212712b
santi12dsd <60608518+santi12dsd@users.noreply.github.com>
2020-02-17 00:20:12 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -3,10 +3,16 @@ A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el anch
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
-
+var largoT;
+var anchoT;
+var CantidadDeAlambre;
+largoT = parseInt(document.getElementById("Largo").value);
+anchoT = parseInt(document.getElementById("Ancho").value);
 function Rectangulo () 
 {
-
+    CantidadDeAlambre = (largoT + largoT + anchoT + anchoT) * 3;
+    alert("La cantidad de alambre a comprar es " + CantidadDeAlambre);
+  
 }
 function Circulo () 
 {
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,43 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
+
+function Sumar() {
+    var Numero1;
+    var Numero2;
+    var Numero3;
+    var Suma;
+
+    Numero1 = parseInt(document.getElementById("PrecioUno").value);
+    Numero2 = parseInt(document.getElementById("PrecioDos").value);
+    Numero3 = parseInt(document.getElementById("PrecioTres").value);
+    Suma = Numero1 + Numero2 + Numero3;
+    alert("El resultado de la suma es " + Suma);
+
 }
-function Promedio () 
-{
-	
+function Promedio() {
+    var Numero1;
+    var Numero2;
+    var Numero3;
+    var Promedio;
+    Numero1 = parseInt(document.getElementById("PrecioUno").value);
+    Numero2 = parseInt(document.getElementById("PrecioDos").value);
+    Numero3 = parseInt(document.getElementById("PrecioTres").value);
+    Promedio = (Numero1 + Numero2 + Numero3) / 3;
+    alert("El promedio es " + Promedio);
+
 }
-function PrecioFinal () 
-{
-	
+function PrecioFinal() {
+    var Numero1;
+    var Numero2;
+    var Numero3;
+    var PrecioFinal;
+    var iva;
+
+    Numero1 = parseInt(document.getElementById("PrecioUno").value);
+    Numero2 = parseInt(document.getElementById("PrecioDos").value);
+    Numero3 = parseInt(document.getElementById("PrecioTres").value);
+    iva = 100 / 21
+    PrecioFinal = iva + (Numero1 + Numero2 + Numero3);
+    alert("El precio final es " + PrecioFinal);
 }
