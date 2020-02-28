f44a1e5587ae885af1b4483923354e42bb9a8860
santi12dsd <60608518+santi12dsd@users.noreply.github.com>
2020-02-27 23:19:38 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+    var largo;
+    var ancho;
+    var perimetro;
+
+    largo = parseInt(prompt("Ingrese el largo"));
+    ancho = parseInt(prompt("Ingrese el ancho"));
+
+    perimetro = largo * 2 + ancho * 2
+    alert("El perimetro es " + perimetro);
+
+    
 
 }
